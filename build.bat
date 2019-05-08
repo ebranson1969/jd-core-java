a35640778945db03806ecb9067854363f89787fb
Eric <ebranson1969@yahoo.com>
2019-05-08 07:24:12 -0500

    Fix

---
+++ build.bat
@@ -0,0 +1,3 @@
+SET JAVA_HOME=C:\DevStudio_2.0.8\java\jdk1.8.0_51
+SET JAVA_HOME_18=C:\DevStudio_2.0.8\java\jdk1.8.0_51
+gradlew assemble
---
+++ jar/setenv.bat
@@ -0,0 +1,4 @@
+SET JAVA_HOME=C:\DevStudio_2.0.8\java\jdk1.8.0_51
+SET JAVA_HOME_18=C:\DevStudio_2.0.8\java\jdk1.8.0_51
+SET FINEOS_GRADLE_ENV=jointDevSunlife
+java -jar jd-core-java-1.2.jar -z -n "C:\Src@SVN\Fineos\DI_Absence\trunk\projects\slf-webapp\build\libs\slf-webapp\WEB-INF\lib"
